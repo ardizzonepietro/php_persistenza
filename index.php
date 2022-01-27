c58d914377149efba0e479a533a695386cb5426d
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-01-27 13:17:49 +0100

    save cookie login

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
 echo "
-<h1>salvo il cookie</h1>";
-$name="1"; //esenziale
-$value="ciao mondo"; //esenziale
+<h1 align = center>accesso cookie</h1>
+<form method=post action=index.php align=center>
+<input type=text name=login id=login required></br>
+<input type=password name=pw id=pw required></br>
+<input type=submit value = invia>
+</form>
+";
+if(!empty($_POST["login"])){
+$name= "login"; //esenziale
+$value=$_POST['login']; //esenziale
 $expire=8600;
 $path="/";
 $domain="";
 $secure="";
 $httpOnly= true;
 setcookie($name,$value, time()+$expire,$path, $domain, $secure,$httpOnly);
+$name= "pw"; //esenziale
+$value=$_POST['pw']; //esenziale
+$expire=8600;
+$path="/";
+$domain="";
+$secure="";
+$httpOnly= true;
+setcookie($name,$value, time()+$expire,$path, $domain, $secure,$httpOnly);
+}
 ?>
