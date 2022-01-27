3e06b5efbadfeeef79dfe2d3aecd67b9881e0b78
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-01-27 13:20:22 +0100

    add index.html

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-echo "
-<h1 align = center>accesso cookie</h1>
-<form method=post action=index.php align=center>
-<input type=text name=login id=login required></br>
-<input type=password name=pw id=pw required></br>
-<input type=submit value = invia>
-</form>
-";
 if(!empty($_POST["login"])){
+
 $name= "login"; //esenziale
 $value=$_POST['login']; //esenziale
 $expire=8600;
@@ -24,5 +17,6 @@ $domain="";
 $secure="";
 $httpOnly= true;
 setcookie($name,$value, time()+$expire,$path, $domain, $secure,$httpOnly);
+echo "<h1 align = center>cookie salvati!</h1>"
 }
 ?>
